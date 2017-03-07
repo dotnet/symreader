9dc1cb2cb4a9491c8c3cccd06b3e0ffbe691fea5
Tomas Matousek <tomat@microsoft.com>
2017-03-06 18:01:44 -0800

    Add IMetadataImport

---
+++ src/Microsoft.DiaSymReader/Shared/ISymUnmanagedSourceServerModule.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace Microsoft.DiaSymReader
+{
+    [ComImport]
+    [Guid("997DD0CC-A76F-4c82-8D79-EA87559D27AD")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    [ComVisible(false)]
+    public interface ISymUnmanagedSourceServerModule
+    {
+        /// <summary>
+        /// Returns the source server data for the module.
+        /// </summary>
+        /// <param name="length">Length of the data.</param>
+        /// <param name="data">
+        /// Pointer to a newly allocated memory containing the data. 
+        /// Caller must free using <see cref="Marshal.FreeCoTaskMem(IntPtr)"/>.
+        /// </param>
+        /// <returns>
+        /// S_OK on success.
+        /// </returns>
+        [PreserveSig]
+        unsafe int GetSourceServerData(out int length, out byte* data);
+    }
+}
