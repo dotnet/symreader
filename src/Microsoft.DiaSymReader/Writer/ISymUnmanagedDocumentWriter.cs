fb2bcc8c357b25f7c6e5e47154cd939400cacaf5
Tomas Matousek <tomat@microsoft.com>
2017-05-22 20:00:52 -0700

    Refactoring

--- src/Microsoft.DiaSymReader/Writer/ISymUnmanagedDocumentWriter.cs
+++ src/Microsoft.DiaSymReader/Writer/ISymUnmanagedDocumentWriter.cs
@@ -8,10 +8,8 @@ using System.Security;
 
 namespace Microsoft.DiaSymReader
 {
-    // TODO:
-
     [ComImport, InterfaceType(ComInterfaceType.InterfaceIsIUnknown), Guid("B01FAFEB-C450-3A4D-BEEC-B4CEEC01E006"), SuppressUnmanagedCodeSecurity]
-    public interface ISymUnmanagedDocumentWriter
+    internal interface ISymUnmanagedDocumentWriter
     {
         void SetSource(uint sourceSize, [MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 0)] byte[] source);
         void SetCheckSum(Guid algorithmId, uint checkSumSize, [MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 1)] byte[] checkSum);
