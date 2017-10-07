2de6ed40382930e5510435fd3577ea57fbf11c5a
Tomas Matousek <tomat@microsoft.com>
2017-10-06 17:30:47 -0700

    Add Reader/Writer factory test coverage

---
+++ src/Microsoft.DiaSymReader.Tests/SymUnmanagedFactoryTests.cs
@@ -0,0 +1,55 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.DiaSymReader.Tools;
+using Roslyn.Test.Utilities;
+using Xunit;
+
+namespace Microsoft.DiaSymReader.Native.UnitTests
+{
+    public class SymUnmanagedFactoryTests
+    {
+        static SymUnmanagedFactoryTests()
+        {
+            Environment.SetEnvironmentVariable("MICROSOFT_DIASYMREADER_NATIVE_ALT_LOAD_PATH", Path.Combine(Path.GetDirectoryName(typeof(SymUnmanagedFactoryTests).GetTypeInfo().Assembly.Location), "DSRN"));
+        }
+
+        [ConditionalFact(typeof(DesktopOnly))]
+        public void Create()
+        {
+            // TODO: Ideally we would run each of these tests in a separate process so they don't interfere with each other.
+            // Native library being loaded makes following loads successful.
+
+            var pdbStream = new MemoryStream(TestResources.SourceLink.WindowsPdb);
+
+            Assert.Throws<DllNotFoundException>(() => 
+                SymUnmanagedReaderFactory.CreateReader<ISymUnmanagedReader5>(pdbStream, DummySymReaderMetadataProvider.Instance, SymUnmanagedReaderCreationOptions.Default));
+
+            Assert.Throws<DllNotFoundException>(() => SymUnmanagedWriterFactory.CreateWriter(DummySymWriterMetadataProvider.Instance));
+            Assert.Throws<DllNotFoundException>(() => SymUnmanagedWriterFactory.CreateWriter(DummySymWriterMetadataProvider.Instance, SymUnmanagedWriterCreationOptions.Deterministic));
+
+            Assert.Throws<NotSupportedException>(() =>
+                SymUnmanagedReaderFactory.CreateReader<ISymUnmanagedReader5>(pdbStream, DummySymReaderMetadataProvider.Instance, SymUnmanagedReaderCreationOptions.UseComRegistry));
+
+            Assert.Throws<SymUnmanagedWriterException>(() => SymUnmanagedWriterFactory.CreateWriter(DummySymWriterMetadataProvider.Instance, 
+                SymUnmanagedWriterCreationOptions.UseComRegistry | SymUnmanagedWriterCreationOptions.Deterministic));
+
+            Assert.NotNull(SymUnmanagedReaderFactory.CreateReader<ISymUnmanagedReader5>(pdbStream, 
+                DummySymReaderMetadataProvider.Instance, SymUnmanagedReaderCreationOptions.UseAlternativeLoadPath));
+
+            Assert.NotNull(SymUnmanagedReaderFactory.CreateReader<ISymUnmanagedReader5>(pdbStream, 
+                DummySymReaderMetadataProvider.Instance, SymUnmanagedReaderCreationOptions.UseAlternativeLoadPath | SymUnmanagedReaderCreationOptions.UseComRegistry));
+
+            Assert.NotNull(SymUnmanagedWriterFactory.CreateWriter(DummySymWriterMetadataProvider.Instance, 
+                SymUnmanagedWriterCreationOptions.UseComRegistry));
+
+            Assert.NotNull(SymUnmanagedWriterFactory.CreateWriter(DummySymWriterMetadataProvider.Instance, 
+                SymUnmanagedWriterCreationOptions.UseAlternativeLoadPath));
+
+            Assert.NotNull(SymUnmanagedWriterFactory.CreateWriter(DummySymWriterMetadataProvider.Instance, 
+                SymUnmanagedWriterCreationOptions.UseAlternativeLoadPath | SymUnmanagedWriterCreationOptions.UseComRegistry | SymUnmanagedWriterCreationOptions.Deterministic));
+        }
+    }
+}
---
+++ src/TestUtilities/DummySymWriterMetadataProvider.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.Reflection;
+
+namespace Microsoft.DiaSymReader.Tools
+{
+    public sealed class DummySymWriterMetadataProvider : ISymWriterMetadataProvider
+    {
+        public static readonly ISymWriterMetadataProvider Instance = new DummySymWriterMetadataProvider();
+
+        public bool TryGetEnclosingType(int nestedTypeToken, out int enclosingTypeToken) 
+            => throw new NotImplementedException();
+
+        public bool TryGetMethodInfo(int methodDefinitionToken, out string methodName, out int declaringTypeToken)
+            => throw new NotImplementedException();
+
+        public bool TryGetTypeDefinitionInfo(int typeDefinitionToken, out string namespaceName, out string typeName, out TypeAttributes attributes)
+            => throw new NotImplementedException();
+    }
+}
