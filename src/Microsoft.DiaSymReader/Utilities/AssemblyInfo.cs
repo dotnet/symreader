3eb0bdde21bb400e5545e31f27c4c7efe68876cb
Tomáš Matoušek <tmat@users.noreply.github.com>
2018-08-09 14:23:38 -0700

    Add TargetFrameworkAtrribute to net20 assembly (#149)

--- src/Microsoft.DiaSymReader/Utilities/AssemblyInfo.cs
+++ src/Microsoft.DiaSymReader/Utilities/AssemblyInfo.cs
@@ -1,4 +1,8 @@
-﻿#if !NET20
+﻿#if NET20
+
+[assembly: System.Runtime.Versioning.TargetFramework(".NETFramework,Version=v2.0")]
+
+#else
 
 [assembly: System.Security.AllowPartiallyTrustedCallers]
 
---
+++ src/Microsoft.DiaSymReader/Utilities/TargetFrameworkAttribute.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+#if NET20
+
+namespace System.Runtime.Versioning
+{
+    [AttributeUsageAttribute(AttributeTargets.Assembly, AllowMultiple = false, Inherited = false)]
+    internal sealed class TargetFrameworkAttribute : Attribute
+    {
+        public string FrameworkName { get; }
+        public string FrameworkDisplayName { get; set; }
+
+        public TargetFrameworkAttribute(string frameworkName) 
+            => FrameworkName = frameworkName;
+    }
+}
+#endif
