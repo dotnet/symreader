dd6b71744be8cdbfd69117e96f5b9e64a7b38b6e
Tomáš Matoušek <tmat@users.noreply.github.com>
2021-08-10 10:09:39 -0700

    Update license to MIT (#248)

--- src/Microsoft.DiaSymReader/Utilities/ExtensionAttribute.cs
+++ src/Microsoft.DiaSymReader/Utilities/ExtensionAttribute.cs
@@ -1,4 +1,7 @@
-﻿#if NET20
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the License.txt file in the project root for more information.
+#if NET20
 namespace System.Runtime.CompilerServices
 {
     /// <summary>
